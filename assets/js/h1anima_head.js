df9ee7dc8ad630b5b25fdd9d27ae517beb051ed5
baokhongnao <hoanglephibao.1212@gmail.com>
2024-05-21 06:50:04 +0700

    create projet TXV

---
+++ assets/js/h1anima_head.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var heading = document.getElementById('animated-heading');
+    heading.style.display = 'block'; // Hiển thị thẻ h1
+  });
+  
+
+  
